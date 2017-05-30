ed71569885861e7be5fcdbe748ffd7e222bd0fde
nikitk00 <nekit1997@meta.ua>
2017-05-30 10:29:33 +0300

    added working gif and layers template

--- views/view_index.php
+++ views/view_index.php
@@ -1,7 +1,8 @@
 <!DOCTYPE HTML>
 <html>
 	<head>
-		<link type="text/css" href="styles/styles.css">
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="styles/styles.css">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
 		<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
@@ -9,7 +10,11 @@
 		<title>Welcome on Nikita's weather app! Choose your city</title>
 	</head>
 	<body>
-		
+		<div class="opsty">
+			<div class="container" id="cntnr">
+				<p>Welcome on my weather website!</p>
+			</div>
+		</div>
 	</body>
 	<footer>
 		
